a3a0842b0d74c36133182719479363c17d2ca287
Juan Jose <inxide@gmail.com>
2018-04-25 00:28:56 +0200

    Nuevo formulario password

---
+++ InstaladorKubo/InstaladorKubo/Password.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormPassword
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormPassword))
+        Me.TbPassword = New System.Windows.Forms.TextBox()
+        Me.LbPassword = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TbPassword
+        '
+        Me.TbPassword.Font = New System.Drawing.Font("Lucida Bright", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TbPassword.ForeColor = System.Drawing.Color.Blue
+        Me.TbPassword.Location = New System.Drawing.Point(25, 67)
+        Me.TbPassword.Name = "TbPassword"
+        Me.TbPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TbPassword.Size = New System.Drawing.Size(232, 43)
+        Me.TbPassword.TabIndex = 0
+        '
+        'LbPassword
+        '
+        Me.LbPassword.AutoSize = True
+        Me.LbPassword.Font = New System.Drawing.Font("Lucida Bright", 18.0!, System.Drawing.FontStyle.Bold)
+        Me.LbPassword.Location = New System.Drawing.Point(35, 18)
+        Me.LbPassword.Name = "LbPassword"
+        Me.LbPassword.Size = New System.Drawing.Size(193, 34)
+        Me.LbPassword.TabIndex = 1
+        Me.LbPassword.Text = "Contraseña:"
+        '
+        'FormPassword
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(281, 125)
+        Me.Controls.Add(Me.LbPassword)
+        Me.Controls.Add(Me.TbPassword)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormPassword"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Control Acceso"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TbPassword As TextBox
+    Friend WithEvents LbPassword As Label
+End Class
---
+++ InstaladorKubo/InstaladorKubo/Password.vb
@@ -0,0 +1,23 @@
+﻿Public Class FormPassword
+
+    'Private Const intentos As Integer = 1
+
+
+
+
+    'Private Sub BtPassword_Click(sender As Object, e As EventArgs) Handles BtPassword.Click
+    '    If TbPassword.Text = "b30330104b" Then
+    '        Me.Hide()
+    '        InstaladorKubo.Show()
+    '    Else
+    '        'Me.Close()
+    '    End If
+    'End Sub
+
+    Private Sub TbPassword_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TbPassword.KeyPress
+        If TbPassword.Text = "b30330104b" Then
+            Me.Hide()
+            InstaladorKubo.Show()
+        End If
+    End Sub
+End Class
