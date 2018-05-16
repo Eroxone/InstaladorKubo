42669eca1326b93eb858bf173008be9bb935ad3a
Juan José López <inxide@gmail.com>
2018-05-16 17:51:19 +0200

    Exploracion inprivate

--- InstaladorKubo/InstaladorKubo/InstaladorKubo.vb
+++ InstaladorKubo/InstaladorKubo/InstaladorKubo.vb
@@ -2330,11 +2330,11 @@ Public Class InstaladorKubo
     End Sub
 
     Private Sub BtISL_Click(sender As Object, e As EventArgs) Handles BtISL.Click
-        MessageBox.Show("Funcionalidad en pruebas. Pendiente de revisión por Sánchez", "Instalar ISL", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        'MessageBox.Show("Funcionalidad en pruebas. Pendiente de revisión por Sánchez", "Instalar ISL", MessageBoxButtons.OK, MessageBoxIcon.Warning)
         FrmConfigurarISL.ShowDialog()
 
-        ' http://isl.notin.net/users/start/ISLAlwaysOn?cmdline=grant_silent+%22zeJw9jjFuAzEMBEEYcZUU%2bYhBiaJEPiHNpUnpRiKp4ADjUtj%2bRt6cMwK4m91mZgIsP7d1e4FvmwdYPr8%2blp1XP0DBMHZsYrNMSUoVSckzenOUFu5CI7cRpQ8sZCqtJFPzntgGz%2f2o2sXJ5ojedWIiqqKcObr0NjRbREidQaHcImc2ra6UtWVC69E6Wy2GCesvXAHW6%2bW0PXJPW9ze4A5wPj%2f3P73D%2fRGPievuyqyFXuEIf6iePyA%3d%22+%2fSILENT+%2fVERYSILENT+password+%22b30330104%22+description+%22carmonas+-+palomo%22
-        ' http://isl.notin.net/start/ISLAlwaysOn?cmdline=grant_silent+%22zeJw9jzFOQzEMQGVVdIKFg1ROHDvxEWAoC2MXx3GqX1W%2fQ%2fuvwZlBHRjfm96bAJ%2bbrZfbHs4%2bd3D8%2bv44vsB5GTsoGM4Da%2fNZZktKgqQ0Mo46sNUYo1HPtUexjoVcWy3J1Ycl9s7zT4haG%2bSzh5lOTETSlDOHNatds0dEkxkUyjVyZlcZSllrJnSLauxSHBPKD9wBlvv1sN4ey3pY4%2fEGG8Dp9M%2bny%2fPkHbZnPXFOIlxJ%2bBX28AtDiT%2fb%22+%2FSILENT+%2FVERYSILENT+password+%22b30330104%22+description+%22carmonas+-+palomo%22
+        ' http://isl.notin.net/users/start/ISLAlwaysOn?cmdline=grant_silent+%22zeJw9jjFuAzEMBEEYcZUU%2bYhBiaJEPiHNpUnpRiKp4ADjUtj%2bRt6cMwK4m91mZgIsP7d1e4FvmwdYPr8%2blp1XP0DBMHZsYrNMSUoVSckzenOUFu5CI7cRpQ8sZCqtJFPzntgGz%2f2o2sXJ5ojedWIiqqKcObr0NjRbREidQaHcImc2ra6UtWVC69E6Wy2GCesvXAHW6%2bW0PXJPW9ze4A5wPj%2f3P73D%2fRGPievuyqyFXuEIf6iePyA%3d%22+%2fSILENT+%2fVERYSILENT+password+%22b30330104%22+description+%22pruebas+-+palomo%22
+
 
         'TODO SANDRAAAAAAAAAAA esto seguro que es cutre y te vas a reir.. como sería bien hecho??
         Dim islnombre As String = FrmConfigurarISL.TbISLNombre.Text
@@ -2346,7 +2346,7 @@ Public Class InstaladorKubo
 
         'Shell("cmd /c " & RutaDescargas & "wget.exe -q --show-progress -t 5 -c ")
 
-        Process.Start("iexplore.exe", "http://isl.notin.net/users/start/ISLAlwaysOn?cmdline=grant_silent+%22zeJw9jjFuAzEMBEEYcZUU%2bYhBiaJEPiHNpUnpRiKp4ADjUtj%2bRt6cMwK4m91mZgIsP7d1e4FvmwdYPr8%2blp1XP0DBMHZsYrNMSUoVSckzenOUFu5CI7cRpQ8sZCqtJFPzntgGz%2f2o2sXJ5ojedWIiqqKcObr0NjRbREidQaHcImc2ra6UtWVC69E6Wy2GCesvXAHW6%2bW0PXJPW9ze4A5wPj%2f3P73D%2fRGPievuyqyFXuEIf6iePyA%3d%22+%2fSILENT+%2fVERYSILENT+password+%22b30330104%22+description+%22" & islgrupo & "+-+" & islnombre & "%22")
+        Process.Start("iexplore.exe", "-private " & "http://isl.notin.net/users/start/ISLAlwaysOn?cmdline=grant_silent+%22zeJw9jjFuAzEMBEEYcZUU%2bYhBiaJEPiHNpUnpRiKp4ADjUtj%2bRt6cMwK4m91mZgIsP7d1e4FvmwdYPr8%2blp1XP0DBMHZsYrNMSUoVSckzenOUFu5CI7cRpQ8sZCqtJFPzntgGz%2f2o2sXJ5ojedWIiqqKcObr0NjRbREidQaHcImc2ra6UtWVC69E6Wy2GCesvXAHW6%2bW0PXJPW9ze4A5wPj%2f3P73D%2fRGPievuyqyFXuEIf6iePyA%3d%22+%2fSILENT+%2fVERYSILENT+password+%22b30330104%22+description+%22" & islgrupo & "+-+" & islnombre & "%22")
 
         'My.Computer.Network.DownloadFile("http://isl.notin.net/users/start/ISLAlwaysOn?cmdline=grant_silent+%22zeJw9jzFOQzEMQGVVdIKFg1ROHDvxEWAoC2MXx3GqX1W%2fQ%2fuvwZlBHRjfm96bAJ%2bbrZfbHs4%2bd3D8%2bv44vsB5GTsoGM4Da%2fNZZktKgqQ0Mo46sNUYo1HPtUexjoVcWy3J1Ycl9s7zT4haG%2bSzh5lOTETSlDOHNatds0dEkxkUyjVyZlcZSllrJnSLauxSHBPKD9wBlvv1sN4ey3pY4%2fEGG8Dp9M%2bny%2fPkHbZnPXFOIlxJ%2bBX28AtDiT%2fb%22+%2fSILENT+%2fVERYSILENT+password+%22b30330104%22+description+%22carmonas+-+palomo%22", RutaDescargas & "ISLAlwaysON.exe", Nothing, Nothing, True, 10000, True)
 
