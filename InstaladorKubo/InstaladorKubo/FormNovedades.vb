261bf3f004c101c8456e1b75dd8bae3354f0725c
Juan Jose <inxide@gmail.com>
2018-10-16 23:23:54 +0200

    Boton Salir en novedades me.close

--- InstaladorKubo/InstaladorKubo/FormNovedades.vb
+++ InstaladorKubo/InstaladorKubo/FormNovedades.vb
@@ -1,5 +1,7 @@
 ﻿Imports Instalador.LeerFicherosINI
 
 Public Class FormNovedades
-
+    Private Sub BtCerrar_Click(sender As Object, e As EventArgs) Handles BtCerrar.Click
+        Me.Close()
+    End Sub
 End Class
