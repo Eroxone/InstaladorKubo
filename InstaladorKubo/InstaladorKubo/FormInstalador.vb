10ec3b1b85f6fad6f09814a39237b41dff40ba74
Juan José López <inxide@hotmail.com>
2018-10-22 17:06:57 +0200

    Creada Clase para ObtenerVersionado

--- InstaladorKubo/InstaladorKubo/FormInstalador.vb
+++ InstaladorKubo/InstaladorKubo/FormInstalador.vb
@@ -10,6 +10,7 @@ Imports Instalador.Chocolatey
 Imports System.Deployment.Application
 Imports Instalador.ODBCNotinSQL
 Imports Instalador.FormNavegador
+Imports Instalador.ObtenerVersionado
 
 'WEB DE INSTALACIÓN
 'http://instalador.notin.net
@@ -22,7 +23,7 @@ Public Class FrmInstaladorKubo
     Private Const TERCEROS_DOWNLOAD As String = "terceros.txt"
     Private Const REGISTRO_DOWNLOAD As String = "registro.txt"
     Private Const PuestoNotin As String = "ftp://ftp.lbackup.notin.net/tecnicos/JUANJO/PuestoNotin/"
-    Private RutaDescargas As String = GetPathTemp() 'PATH_TEMP
+    Public RutaDescargas As String = GetPathTemp() 'PATH_TEMP
     Public Const instaladorkuboini = "C:\TEMP\InstaladorKubo\InstaladorKubo.ini"
 
 
@@ -4272,7 +4273,7 @@ Public Class FrmInstaladorKubo
                 Dim notinnet As Process = Process.Start(pnotinnet)
                 notinnet.WaitForExit()
                 RegistroInstalacion("ÉXITO: NOTIN NET ejecutado correctamente desde F:\Notawin.Net tras la descarga de Notin8.exe.")
-                ObtenerVersionNotin()
+                ObtenerVersionado.ObtenerVersionNotin()
                 ObtenerVersionNet()
                 ObtenerVersionMigrador()
             Catch ex As Exception
@@ -4302,169 +4303,12 @@ Public Class FrmInstaladorKubo
         BtNotin8exe.BackColor = SystemColors.Control
     End Sub
 
-    'Private Sub ObtenerVersionNotin()
-    '    Try
-    '        Dim lognotin As String = "C:\Notawin.Net\Notin.log"
-    '        Dim infoversion As String
-
-    '        Dim sr As New System.IO.StreamReader(lognotin)
-
-    '        While sr.ReadLine.Contains("ACCESS=ON")
-    '            infoversion = sr.ReadLine()
-    '        End While
-
-
-    '        sr.Close()
-    '        Dim numeroversion = infoversion.LastIndexOf(":")
-    '        infoversion = infoversion.Substring(numeroversion + 2)
-    '        LbVersionNotin.Text = infoversion
-    '        cIniArray.IniWrite(instaladorkuboini, "NOTIN", "VERSION", infoversion)
-    '        RegistroInstalacion("NOTIN: InfoVersión en el Sistema: " & infoversion)
-    '    Catch ex As Exception
-    '        RegistroInstalacion("NOTIN: No se pudo determinar Versión NOTIN en Sistema.")
-    '    End Try
-    'End Sub
-
-
-    Public Sub ObtenerVersionNotin()
-        Dim criterio As String
-        Dim filename As String
-
-        Dim contador As Integer = 1
-        Dim Linea_Actual As Integer = 0
-
-        ' Obtener el resultado
-        Dim vlcResultado As String = ""
-        ' Especifica el criterio
-        criterio = "ACCESS=ON"
-        Dim vlcCriterio As String = criterio
-        ' Nombre del archivo a utilizar
-        filename = "C:\Notawin.Net\Notin.log"
-        Dim vlcFileName As String = filename
-        ' Si el archivo existe
-        If IO.File.Exists(vlcFileName) Then
-            ' Crear el stream
-            Dim vloFile As New StreamReader(vlcFileName)
-            ' Ciclo de iteración entre líneas
-            While True
-                ' Cargar la línea
-                Dim vlcLinea = vloFile.ReadLine()
-                ' Si la línea es nula, sale del ciclo
-                If vlcLinea Is Nothing Then Exit While
-                ' Si la línea contiene el criterio
-                If vlcLinea.Contains(vlcCriterio) Then
-                    ' Obtener la línea como resultado,
-                    ' Concatenando un retorno de carro si ya hay ocurrencias
-                    vlcResultado += IIf(vlcResultado <> "", vbCrLf, "") & vlcLinea
-                    Linea_Actual = contador
-                    ' Salir del ciclo
-                    'Exit While ' Lo omites si quieres todas las ocurrencias
-                End If
-                contador += 1
-
-            End While
-        End If
-        ' Si no se encontraron resultados
-        If vlcResultado = "" Then
-            vlcResultado = "Sin información"
-            LbVersionNotin.Text = vlcResultado
-            Exit Sub
-        End If
-
-        'MsgBox("El resultado de La busqueda es: " + vlcResultado.ToString + " La linea en la que lo encontro es: " + Linea_Actual.ToString)
-
-        Try
-            Dim numeroversion = vlcResultado.LastIndexOf(":")
-            vlcResultado = vlcResultado.Substring(numeroversion + 2)
-            LbVersionNotin.Text = vlcResultado
-            cIniArray.IniWrite(instaladorkuboini, "NOTIN", "VERSION", vlcResultado)
-            RegistroInstalacion("NOTIN: InfoVersión en el Sistema: " & vlcResultado)
-        Catch ex As Exception
-            'RegistroInstalacion("NOTIN: No se pudo determinar Versión NOTIN en Sistema.")
-        End Try
-
-    End Sub
-
-    'TODO Mover todos los de Obtener version a una Clase
-
-    Private Sub ObtenerVersionMigrador()
-        Try
-            Dim migradorok = cIniArray.IniGet("F:\WINDOWS\NNOTIN.INI", "VARIABLES", "VersionMigradorNotinSQL", "0")
-            If migradorok <> 0 Then
-                LbMigradorINI.Text = "MigradorSQL " & migradorok
-            End If
-        Catch ex As Exception
-            RegistroInstalacion("No se pudo obtener versión de MigradorSQL. " & ex.Message)
-        End Try
-
-    End Sub
-
-
-
-    Private Sub ObtenerVersionNet()
-        Try
-            Dim appData As String = GetFolderPath(SpecialFolder.ApplicationData)
-            Dim infoinstaller As String = appData & "\Notin\InfoInstaller.txt"
-            Dim infoversion As String
-
-            Dim sr As New System.IO.StreamReader(infoinstaller)
-            infoversion = sr.ReadLine()
-            'infoversion = sr.ReadToEnd
-            sr.Close()
-            Dim numeroversion = infoversion.LastIndexOf(",")
-            infoversion = infoversion.Substring(0,numeroversion)
-            LbBetaNet.Text = infoversion
-            cIniArray.IniWrite(instaladorkuboini, "NET", "NETSISTEMA", infoversion)
-            'cIniArray.IniWrite(instaladorkuboini, "NET", "FECHAEJECUCION", DateTime.Now)
-            RegistroInstalacion("NOTIN .NET: InfoVersión en el Sistema: " & infoversion)
-        Catch ex As Exception
-            RegistroInstalacion("NOTIN .NET: No se pudo determinar Versión .Net en Sistema.")
-        End Try
-    End Sub
 
     Private Sub BtVersionFW_Click(sender As Object, e As EventArgs) Handles BtVersionFW.Click
         ObtenerVersionFW()
         BtEstablew32F462.Enabled = True
         BtNetBetaW32F462.Enabled = True
     End Sub
-
-    Private Sub ObtenerVersionFW()
-        Directory.CreateDirectory(RutaDescargas)
-
-        Try
-            Shell("cmd /c reg Query " & """" & "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Client" & """" & " /v Version > " & RutaDescargas & "fwversion.txt", AppWinStyle.Hide, True)
-        Catch ex As Exception
-            RegistroInstalacion("ERROR Obtener versión FW: " & ex.Message)
-        End Try
-
-        Try
-            Dim versionfw As String = RutaDescargas & "fwversion.txt"
-            Dim infoversion As String = "Sin información."
-
-            Dim sr As New System.IO.StreamReader(versionfw)
-            Dim numerolinea As Integer = 0
-            'TODO Sandraaaaaaaa ayudame con esto. While seguro es cutre jaja
-            While numerolinea < 3
-                infoversion = sr.ReadLine()
-                numerolinea = numerolinea + 1
-            End While
-            sr.Close()
-
-            TlpVersionFW.ToolTipTitle = "REG Query para Versión Framework:"
-            TlpVersionFW.SetToolTip(BtVersionFW, infoversion)
-
-            Dim numeroversion As String = infoversion.Substring(25)
-
-            LbVersionFW.Text = "Versión " & numeroversion
-
-            cIniArray.IniWrite(instaladorkuboini, "NET", "FWSISTEMA", numeroversion)
-            RegistroInstalacion("INFO FRAMEWORK: InfoVersión en el Sistema: " & numeroversion)
-            File.Delete(RutaDescargas & "fwversion.txt")
-        Catch ex As Exception
-            RegistroInstalacion("INFO FRAMEWORK: No se pudo determinar Versión FW en Sistema.")
-        End Try
-    End Sub
-
 
 
     Private Sub BtNetBeta_Click(sender As Object, e As EventArgs) Handles BtNetBeta.Click
---
+++ InstaladorKubo/InstaladorKubo/ObtenerVersionado.vb
@@ -0,0 +1,136 @@
+﻿Imports Instalador.FrmInstaladorKubo
+Imports System.IO
+Imports Instalador.LeerFicherosINI
+Imports System.Environment
+
+Public Class ObtenerVersionado
+
+    Public Shared Sub ObtenerVersionNotin()
+        Dim criterio As String
+        Dim filename As String
+
+        Dim contador As Integer = 1
+        Dim Linea_Actual As Integer = 0
+
+        ' Obtener el resultado
+        Dim vlcResultado As String = ""
+        ' Especifica el criterio
+        criterio = "ACCESS=ON"
+        Dim vlcCriterio As String = criterio
+        ' Nombre del archivo a utilizar
+        filename = "C:\Notawin.Net\Notin.log"
+        Dim vlcFileName As String = filename
+        ' Si el archivo existe
+        If IO.File.Exists(vlcFileName) Then
+            ' Crear el stream
+            Dim vloFile As New StreamReader(vlcFileName)
+            ' Ciclo de iteración entre líneas
+            While True
+                ' Cargar la línea
+                Dim vlcLinea = vloFile.ReadLine()
+                ' Si la línea es nula, sale del ciclo
+                If vlcLinea Is Nothing Then Exit While
+                ' Si la línea contiene el criterio
+                If vlcLinea.Contains(vlcCriterio) Then
+                    ' Obtener la línea como resultado,
+                    ' Concatenando un retorno de carro si ya hay ocurrencias
+                    vlcResultado += IIf(vlcResultado <> "", vbCrLf, "") & vlcLinea
+                    Linea_Actual = contador
+                    ' Salir del ciclo
+                    'Exit While ' Lo omites si quieres todas las ocurrencias
+                End If
+                contador += 1
+
+            End While
+        End If
+        ' Si no se encontraron resultados
+        If vlcResultado = "" Then
+            vlcResultado = "Sin información"
+            FrmInstaladorKubo.LbVersionNotin.Text = vlcResultado
+            Exit Sub
+        End If
+
+        'MsgBox("El resultado de La busqueda es: " + vlcResultado.ToString + " La linea en la que lo encontro es: " + Linea_Actual.ToString)
+
+        Try
+            Dim numeroversion = vlcResultado.LastIndexOf(":")
+            vlcResultado = vlcResultado.Substring(numeroversion + 2)
+            FrmInstaladorKubo.LbVersionNotin.Text = vlcResultado
+            cIniArray.IniWrite(instaladorkuboini, "NOTIN", "VERSION", vlcResultado)
+            RegistroInstalacion("NOTIN: InfoVersión en el Sistema: " & vlcResultado)
+        Catch ex As Exception
+            'RegistroInstalacion("NOTIN: No se pudo determinar Versión NOTIN en Sistema.")
+        End Try
+    End Sub
+
+
+    Public Shared Sub ObtenerVersionNet()
+        Try
+            Dim appData As String = GetFolderPath(SpecialFolder.ApplicationData)
+            Dim infoinstaller As String = appData & "\Notin\InfoInstaller.txt"
+            Dim infoversion As String
+
+            Dim sr As New System.IO.StreamReader(infoinstaller)
+            infoversion = sr.ReadLine()
+            'infoversion = sr.ReadToEnd
+            sr.Close()
+            Dim numeroversion = infoversion.LastIndexOf(",")
+            infoversion = infoversion.Substring(0, numeroversion)
+            FrmInstaladorKubo.LbBetaNet.Text = infoversion
+            cIniArray.IniWrite(instaladorkuboini, "NET", "NETSISTEMA", infoversion)
+            'cIniArray.IniWrite(instaladorkuboini, "NET", "FECHAEJECUCION", DateTime.Now)
+            RegistroInstalacion("NOTIN .NET: InfoVersión en el Sistema: " & infoversion)
+        Catch ex As Exception
+            RegistroInstalacion("NOTIN .NET: No se pudo determinar Versión .Net en Sistema.")
+        End Try
+    End Sub
+
+    Public Shared Sub ObtenerVersionMigrador()
+        Try
+            Dim migradorok = cIniArray.IniGet("F:\WINDOWS\NNOTIN.INI", "VARIABLES", "VersionMigradorNotinSQL", "0")
+            If migradorok <> 0 Then
+                FrmInstaladorKubo.LbMigradorINI.Text = "MigradorSQL " & migradorok
+            End If
+        Catch ex As Exception
+            RegistroInstalacion("No se pudo obtener versión de MigradorSQL. " & ex.Message)
+        End Try
+    End Sub
+
+    Public Shared Sub ObtenerVersionFW()
+        Directory.CreateDirectory(FrmInstaladorKubo.RutaDescargas)
+
+        Try
+            Shell("cmd /c reg Query " & """" & "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Client" & """" & " /v Version > " & FrmInstaladorKubo.RutaDescargas & "fwversion.txt", AppWinStyle.Hide, True)
+        Catch ex As Exception
+            RegistroInstalacion("ERROR Obtener versión FW: " & ex.Message)
+        End Try
+
+        Try
+            Dim versionfw As String = FrmInstaladorKubo.RutaDescargas & "fwversion.txt"
+            Dim infoversion As String = "Sin información."
+
+            Dim sr As New System.IO.StreamReader(versionfw)
+            Dim numerolinea As Integer = 0
+            'TODO Sandraaaaaaaa ayudame con esto. While seguro es cutre jaja
+            While numerolinea < 3
+                infoversion = sr.ReadLine()
+                numerolinea = numerolinea + 1
+            End While
+            sr.Close()
+
+            FrmInstaladorKubo.TlpVersionFW.ToolTipTitle = "REG Query para Versión Framework:"
+            FrmInstaladorKubo.TlpVersionFW.SetToolTip(FrmInstaladorKubo.BtVersionFW, infoversion)
+
+            Dim numeroversion As String = infoversion.Substring(25)
+
+            FrmInstaladorKubo.LbVersionFW.Text = "Versión " & numeroversion
+
+            cIniArray.IniWrite(instaladorkuboini, "NET", "FWSISTEMA", numeroversion)
+            RegistroInstalacion("INFO FRAMEWORK: InfoVersión en el Sistema: " & numeroversion)
+            File.Delete(FrmInstaladorKubo.RutaDescargas & "fwversion.txt")
+        Catch ex As Exception
+            RegistroInstalacion("INFO FRAMEWORK: No se pudo determinar Versión FW en Sistema.")
+        End Try
+    End Sub
+
+End Class
